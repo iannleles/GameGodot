1788f23fb9a6a0b236e0a21e40beec427ea8c588
Iann Leles <iannleles@outlook.com>
2020-09-04 00:52:25 -0300

    Criando jogo

---
+++ Player.gd
@@ -0,0 +1,21 @@
+extends KinematicBody2D
+
+const UP = Vector2(0, -1)
+var motion = Vector2()
+
+func _physics_process(delta):
+	
+	motion.y += 10
+	
+	if Input.is_action_pressed("ui_right"):
+		motion.x = 100
+	elif Input.is_action_pressed("ui_left"):
+		motion.x = -100
+	else:
+		motion.x = 0
+		
+	if is_on_floor():
+		print("esta no chao")
+		if Input.is_action_pressed("ui_up"):
+			motion.y = -300
+	move_and_slide(motion, UP)
